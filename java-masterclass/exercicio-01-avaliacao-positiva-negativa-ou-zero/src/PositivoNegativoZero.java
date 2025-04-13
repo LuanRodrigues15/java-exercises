36c1d81daf39ae2090f1079f79454fa003dbc5b8
Luan Rodrigues <luan15008090@gmail.com>
2025-04-13 17:23:07 -0300

    Deletando tudo

--- java-masterclass/exercicio-01-avaliacao-positiva-negativa-ou-zero/src/PositivoNegativoZero.java
+++
@@ -1,5 +0,0 @@
-public class PositivoNegativoZero {
-    public static void checkNumber(int number){
-        System.out.println((number > 0 ? "Positivo" : number < 0 ? "Negativo" : "Zero"));
-    }
-}
