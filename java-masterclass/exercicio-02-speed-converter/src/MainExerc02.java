9f72bd9238e70f74e74746b5af8e60a10a5178df
Luan Rodrigues <luan15008090@gmail.com>
2025-04-13 16:52:46 -0300

    Removendo exercício 2

--- java-masterclass/exercicio-02-speed-converter/src/MainExerc02.java
+++
@@ -1,9 +0,0 @@
-public class MainExerc02 {
-    public static void main(String[] args) {
-        SpeedConverter.printConversion(1.5);
-        SpeedConverter.printConversion(10.25);
-        SpeedConverter.printConversion(-5.6);
-        SpeedConverter.printConversion(25.42);
-        SpeedConverter.printConversion(75.114);
-    }
-}
