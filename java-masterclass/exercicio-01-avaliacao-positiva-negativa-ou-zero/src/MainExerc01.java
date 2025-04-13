e2459cb42591a82bdcad1fb1b2f1aa72f28b8d07
Luan Rodrigues <luan15008090@gmail.com>
2025-04-13 18:00:59 -0300

    Add. Exerc 1 e 2

---
+++ java-masterclass/exercicio-01-avaliacao-positiva-negativa-ou-zero/src/MainExerc01.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MainExerc01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        PositiveNegativeZero.checkNumber(sc.nextInt());
+    }
+}
---
+++ java-masterclass/exercicio-01-avaliacao-positiva-negativa-ou-zero/src/PositiveNegativeZero.java
@@ -0,0 +1,6 @@
+public class PositiveNegativeZero {
+    public static void checkNumber(int number) {
+        System.out.println("The number is");
+        System.out.println((number > 0 ? "positive" : number < 0 ? "negative" : "zero"));
+    }
+}
